53dc46408fc13b253e3d101d9834eb9f555f1e29
akhand2210093 <akhandpratapsingh11c@gmail.com>
2025-03-31 06:33:38 +0530

    application setting

--- ai_query_engine/settings.py
+++ ai_query_engine/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'rest_framework',
+    'query',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
